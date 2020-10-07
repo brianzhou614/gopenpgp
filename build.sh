5b9c8bd4cc43ccce16e5cd682af96a20eb58eaea
Marin Thiercelin <marin.thiercelin@pm.me>
2020-10-07 11:51:21 +0200

    build file includes macos and macos-ui

--- build.sh
+++ build.sh
@@ -19,14 +19,31 @@ ANDROID_OUT_FILE_NAME="gopenpgp"
 ANDROID_OUT_FILE=${ANDROID_OUT}/${ANDROID_OUT_FILE_NAME}.aar
 ANDROID_JAVA_PAG="com.proton.${ANDROID_OUT_FILE_NAME}"
 
+IOS_AND_SIM_OUT=${OUTPUT_PATH}/"iOS_and_Simulator"
+IOS_AND_SIM_OUT_FILE_NAME="Crypto"
+IOS_AND_SIM_OUT_FILE=${IOS_AND_SIM_OUT}/${IOS_AND_SIM_OUT_FILE_NAME}.framework
+
 IOS_OUT=${OUTPUT_PATH}/"iOS"
 IOS_OUT_FILE_NAME="Crypto"
 IOS_OUT_FILE=${IOS_OUT}/${IOS_OUT_FILE_NAME}.framework
 
+macOS_OUT=${OUTPUT_PATH}/"macOS"
+macOS_OUT_FILE_NAME="Crypto"
+macOS_OUT_FILE=${macOS_OUT}/${macOS_OUT_FILE_NAME}.framework
 
+macOSUI_OUT=${OUTPUT_PATH}/"macOSUI"
+macOSUI_OUT_FILE_NAME="Crypto"
+macOSUI_OUT_FILE=${macOSUI_OUT}/${macOSUI_OUT_FILE_NAME}.framework
+
+IOSSIM_OUT=${OUTPUT_PATH}/"iOS_Simulator"
+IOSSIM_OUT_FILE_NAME="Crypto"
+IOSSIM_OUT_FILE=${IOSSIM_OUT}/${IOSSIM_OUT_FILE_NAME}.framework
 
 mkdir -p $ANDROID_OUT
 mkdir -p $IOS_OUT
+mkdir -p $macOS_OUT
+mkdir -p $macOSUI_OUT
+mkdir -p $IOSSIM_OUT
 
 install() 
 {
@@ -69,7 +86,7 @@ external()
 ######## MARK -- Main
 
 #flags
-DFLAGS="-s -w"
+DFLAGS="'all=-s -w'"
 
 PACKAGES=""
 #add internal package 
@@ -93,13 +110,29 @@ printf "PACKAGES: ${PACKAGES}\n"
 
 printf "\e[0;32mStart Building iOS framework .. Location: ${IOS_OUT} \033[0m\n\n"
 ## tags - mobile tag will filter unsupported functions  //ios macos macos-ui
-gomobile bind -tags mobile -target ios -x -o ${IOS_OUT_FILE} -ldflags="${DFLAGS}" ${PACKAGES}
+gomobile bind -tags mobile -target ios -x -o ${IOS_AND_SIM_OUT_FILE} -ldflags="${DFLAGS}" ${PACKAGES}
 # install iOS  ${IOS_OUT_FILE} ${IOS_PROJECT_PATH}
 
-printf "\e[0;32mStart Building Android lib .. Location: ${ANDROID_OUT} \033[0m\n\n"
-# gomobile bind -tags mobile -target android -javapkg ${ANDROID_JAVA_PAG} -o ${ANDROID_OUT_FILE} -ldflags="${DFLAGS}" ${PACKAGES}
+printf "\e[0;32mStart Building macOS framework .. Location: ${macOS_OUT} \033[0m\n\n"
+gomobile bind -tags mobile -target macos -x -o ${macOS_OUT_FILE} -ldflags="${DFLAGS}" ${PACKAGES}
+
+printf "\e[0;32mStart Building macOS-UI framework .. Location: ${macOSUI_OUT} \033[0m\n\n"
+gomobile bind -tags mobile -target macos-ui -x -o ${macOSUI_OUT_FILE} -ldflags="${DFLAGS}" ${PACKAGES}
+
+#printf "\e[0;32mStart Building Android lib .. Location: ${ANDROID_OUT} \033[0m\n\n"
+#gomobile bind -tags mobile -target android -javapkg ${ANDROID_JAVA_PAG} -o ${ANDROID_OUT_FILE} -ldflags="${DFLAGS}" ${PACKAGES}
 # install Android ${ANDROID_OUT} ${ANDROID_PROJECT_PATH}
 
 printf "\e[0;32mInstalling frameworks. \033[0m\n\n"
 
 printf "\e[0;32mAll Done. \033[0m\n\n"
+
+cp -rL $IOS_AND_SIM_OUT_FILE $IOS_OUT_FILE;
+cp -rL $IOS_AND_SIM_OUT_FILE $IOSSIM_OUT_FILE;
+
+lipo $IOSSIM_OUT_FILE/Versions/A/Crypto -remove arm64 -output $IOSSIM_OUT_FILE/Versions/A/Crypto;
+lipo $IOS_OUT_FILE/Versions/A/Crypto -remove x86_64 -output $IOS_OUT_FILE/Versions/A/Crypto;
+
+XCFRAMEWORK_OUT_FILE=$OUTPUT_PATH/Crypto.xcframework
+xcodebuild -create-xcframework  -framework $IOS_OUT_FILE  -framework $macOS_OUT_FILE -framework $macOSUI_OUT_FILE -framework $IOSSIM_OUT_FILE  -output $XCFRAMEWORK_OUT_FILE;
+zip -r $XCFRAMEWORK_OUT_FILE.zip $XCFRAMEWORK_OUT_FILE;
